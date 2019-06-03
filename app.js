37981a8f3ef8bc56d4c17bbb8f70f9bd4af8e5ae
Ripetaurus <lactic.nakul@gmail.com>
2019-06-03 21:30:36 +0530

    home.ejs setup

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
 
+var publicDir = require('path').join(__dirname, '/public');
+app.use(express.static(publicDir));
+
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res) {
-     res.send('This is the root route');
+	res.render('home');
 });
 
 app.listen(8080, function() {
-     console.log('Server started on localhost:8080');
+	console.log('Server started on localhost:8080');
 });
-
-
